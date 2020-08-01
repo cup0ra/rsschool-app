d7cdb5da0cc3e16f0d090de64baba8199f2ae7cf
AlreadyBored <shilaums@gmail.com>
2020-08-01 23:59:07 +0300

    feat: make component that partially recieves necessary data (draft)

--- client/src/pages/cv/index.tsx
+++ client/src/pages/cv/index.tsx
@@ -3,6 +3,14 @@ import { Layout, Typography } from 'antd';
 import { NextRouter, withRouter } from 'next/router';
 import withSession, { Session } from 'components/withSession';
 import { LoadingScreen } from 'components/LoadingScreen';
+import { UserService } from 'services/user';
+import {
+  Contacts,
+  EmploymentRecord,
+  CoursesStats,
+  PublicFeedback,
+  CoreCVInfo
+} from '../../../../common/models/cv';
 
 const { Content } = Layout;
 const { Title } = Typography;
@@ -14,12 +22,63 @@ type Props = {
 
 type State = {
   isLoading: boolean;
+  coreInfo: CoreCVInfo | null;
+  contacts: Contacts | null;
+  badges: PublicFeedback[] | null;
+  educationHistory: any | null;
+  employmentHistory: EmploymentRecord[] | null;
+  coursesStats: CoursesStats[] | null;
 };
 
 class CVPage extends React.Component<Props, State> {
   state: State = {
-    isLoading: false
+    isLoading: false,
+    coreInfo: null,
+    contacts: null,
+    badges: null,
+    educationHistory: null,
+    employmentHistory: null,
+    coursesStats: null,
   };
+
+  private async getData() {
+    await this.setState({ isLoading: true });
+    const { router } = this.props;
+
+    try {
+      const githubId = router.query ? (router.query.githubId as string) : undefined;
+      const profile = await this.userService.getProfileInfo(githubId);
+/*       const {
+        contacts,
+        generalInfo: {
+          aboutMyself: about,
+          educationHistory,
+          englishLevel,
+          githubId: github,
+          location,
+          name
+        }
+      } = profile; */
+      console.log(profile);
+
+  } catch(e) {
+    await this.setState({
+      isLoading: false,
+      coreInfo: null,
+      contacts: null,
+      badges: null,
+      educationHistory: null,
+      employmentHistory: null,
+      coursesStats: null,
+    });
+  }
+}
+
+  private userService = new UserService();
+
+  async componentDidMount() {
+    await this.getData();
+  }
 
 render() {
 
